9d5fe1b92ebb0d42edc752d18091188838b25c48
Valdielson Siqueira <valdielson.silva@gmail.com>
2021-12-14 23:14:44 -0300

    feat(tech): usando hook useState aula 03

---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+export default function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+   setCounter(counter + 1);
+  }
+  return (
+    <div>
+      <h2>{counter}</h2>
+      <button type="button" onClick={increment}>
+        Incremend
+      </button>
+    </div>
+  )
+}
